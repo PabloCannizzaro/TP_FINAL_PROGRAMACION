203e614e0be3ab46363005d688267e28dec23c92
PabloCannizzaro <pablocannizzaro14@gmail.com>
2025-10-17 09:10:04 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from pathlib import Path
 from .api import register_routes
 
+BASE_DIR = Path(__file__).resolve().parents[1]  # carpeta raíz del proyecto
+
 def create_app():
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        template_folder=str(BASE_DIR / "templates"),
+        static_folder=str(BASE_DIR / "static"),
+    )
     register_routes(app)
     return app
